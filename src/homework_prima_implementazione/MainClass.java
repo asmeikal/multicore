a4badc24935afa9b3505c06ddabacbacd1ce68e6
Michele Laurenti <asmeikal@me.com>
2015-12-01 09:42:46 +0100

    Clean formatting

--- src/homework_prima_implementazione/MainClass.java
+++ src/homework_prima_implementazione/MainClass.java
@@ -11,14 +11,14 @@ public class MainClass
 		// prende in input numero processori e lunghezza parole
 		int n_threads = Integer.parseInt(args[0]);
 		int length = Integer.parseInt(args[1]);
-		
+
 		MD5 md5 = new MD5();
 		Random r = new Random();
 		String alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
 		HashSet<String> passwords = new HashSet<String>();
 
 		passwords.add(md5.hash("99999"));
-		
+
 		for (int i = 0; i < 5; ++i) {
 			StringBuilder s = new StringBuilder();
 			for (int j = 0; j < 5; ++j) {
@@ -26,7 +26,7 @@ public class MainClass
 			}
 			passwords.add(md5.hash(s.toString()));
 		}
-		
+
 		System.out.println(passwords);
 
 		Master m = new Master(passwords, n_threads, length);
--- src/homework_prima_implementazione/Master.java
+++ src/homework_prima_implementazione/Master.java
@@ -9,7 +9,7 @@ public class Master
 	// hashes - password da trovare
 	private HashSet<String> hashes;
 
-    // passwordMap - hash -> password trovata
+	// passwordMap - hash -> password trovata
 	private Map<String, String> passwordMap;
 
 	private AllStringGenerator asg;
@@ -19,7 +19,6 @@ public class Master
 	private int length;
 
 	private GeneratorFactory gf;
-
 
 	/**
 	 * Parametrizzare: numero di thread; lunghezza password da dare ai
@@ -31,41 +30,41 @@ public class Master
 		this.workerThreads = new WorkerThread[n_thread];
 		this.length = length;
 
-		this.gf = new GeneratorFactory("abcdefghijklmnopqrstuvwxyz0123456789",this.length);
+		this.gf = new GeneratorFactory("abcdefghijklmnopqrstuvwxyz0123456789", this.length);
 
 		this.asg = this.gf.getASG();
 		this.length = length;
-        this.passwordMap = new HashMap<String, String>();
+		this.passwordMap = new HashMap<String, String>();
 
 	}
 
-    public void work() throws InterruptedException
+	public void work() throws InterruptedException
 	{
-        MD5 md5 = new MD5();
+		MD5 md5 = new MD5();
 
 		// generare le prime parole e controllarle
 		for (int i = 1; i <= this.length; ++i) {
 			StringGenerator sg = gf.getSG(i);
 			while (sg.hasWords()) {
-                String w = sg.getNextWord();
-                String hash = md5.hash(w);
-                if (this.hashes.contains(hash)) {
-                    this.foundPassword(hash,w);
+				String w = sg.getNextWord();
+				String hash = md5.hash(w);
+				if (this.hashes.contains(hash)) {
+					this.foundPassword(hash, w);
 				}
-            }
+			}
 		}
 		for (int i = 0; i < this.workerThreads.length; i++) {
 			workerThreads[i] = new WorkerThread(this, this.asg, gf.getSG());
 			workerThreads[i].start();
 		}
-		for (int i=0; i < workerThreads.length; i++) {
+		for (int i = 0; i < workerThreads.length; i++) {
 			workerThreads[i].join();
 		}
 
 		System.out.println(this.passwordMap);
 	}
 
-    synchronized public void foundPassword(String hash, String pass)
+	synchronized public void foundPassword(String hash, String pass)
 	{
 		this.hashes.remove(hash);
 		this.passwordMap.put(hash, pass);
@@ -76,7 +75,7 @@ public class Master
 	{
 		// ritorna un hash set di password ancora da trovare
 		HashSet<String> copy = new HashSet<String>();
-        copy.addAll(this.hashes);
+		copy.addAll(this.hashes);
 		return copy;
 	}
 }
