7cad5d98521a10b522cefd98f4e02bc3c28cfd9a
Gabriele Atria <gabriele.atria@gmail.com>
2015-12-02 19:21:41 +0100

    Update WorkerThread.java

--- src/homework_seconda_implementazione/WorkerThread.java
+++ src/homework_seconda_implementazione/WorkerThread.java
@@ -3,17 +3,39 @@ package homework_seconda_implementazione;
 
 public class WorkerThread
 {
-	// stessi metodi, reimplementare allocando meno oggetti possibili
+    // stessi metodi, reimplementare allocando meno oggetti possibili
+    
+    private Master master;
+    private AllStringGenerator asg;
+    private StringGenerator sg;
+    private MD5 hasher;
 
-	public void run()
-	{
-		// this.length = this.sg.length
-		// byte[] radice = this.asg.getNextWord();
-		// byte[] parola = new byte[radice.length() + this.length];
-		// Arrays.copy(parola, radice);
-		// while (sg.hasWords()) {
-		// sg.getNextWord(parola, radice.length());
-		// byte[] hash = md5.hash(parola);
-		// }
-	}
+    public WorkerThread (Master m, AllStringGenerator asg, StringGenerator sg) {
+        this.master = m;
+        this.asg = asg;
+        this.sg = sg;
+        this.hasher = new MD5();
+    }
+
+        public void run()
+    {
+        HashSet<byte[]> passwords = this.master.getPasswords();
+        byte[] root;
+        byte[] word;
+        while (!passwords.isEmpty()) {
+            root = this.asg.getNextWord();
+            word = Arrays.copyOf(root,root.length + this.sg.length);
+            while (this.sg.hasWords()) {
+                sg.getNextWord(word,root.length);
+                byte[] hash = this.hasher.hash(word);
+                for (byte[] password : passwords) {
+                    if (Arrays.equals(password,hash)) {
+                        this.master.foundPassword(hash, word);
+                    }
+                }
+            }
+            this.sg.reset();
+            passwords = this.master.getPasswords();
+        }
+    }
 }
