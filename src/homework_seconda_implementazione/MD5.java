46e29fb094660a954a1da45139cf03e87336ee3f
Michele Laurenti <asmeikal@me.com>
2015-12-02 16:14:21 +0100

    Create MD5 class

--- src/homework_seconda_implementazione/MD5.java
+++ src/homework_seconda_implementazione/MD5.java
@@ -1,10 +1,24 @@
 package homework_seconda_implementazione;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MD5
 {
-	public MessageDigest hasher;
+	private MessageDigest hasher;
 
-	public byte[] hash(byte pass[]);
+	public MD5()
+	{
+		try {
+			this.hasher = MessageDigest.getInstance("md5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Impossibile creare hasher md5.");
+		}
+	}
+
+	public byte[] hash(byte[] pass)
+	{
+		hasher.update(pass);
+		return this.hasher.digest();
+	}
 }
