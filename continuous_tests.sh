be9cff9ce1c7c7ddfe1de18d15d9b15d78375021
Michele Laurenti <asmeikal@me.com>
2015-12-04 13:30:13 +0100

    Update to tester script

--- continuous_tests.sh
+++ continuous_tests.sh
@@ -4,8 +4,8 @@ BIN="./bin"
 
 source jvms.sh
 
-prima="homework_prima_implementazione/MainClass.class"
-seconda="homework_seconda_implementazione/MainClass.class"
+prima="homework_prima_implementazione/MainClass"
+seconda="homework_seconda_implementazione/MainClass"
 
 wait_min() {
 	secs=$(($1 * 60))
@@ -20,11 +20,15 @@ wait_min() {
 
 while read impl jvm args; do
 	if [[ ! $impl ]] ; then continue ; fi
-	wait_min 1
 	echo ""
-	echo "$impl $jvm $args"
 	time=`date +"%F-%H-%M"`
 	fname="${impl}_${jvm}_${time}.log"
+	if [ -e "./$fname" ] ; then
+		wait_min 1
+		time=`date +"%F-%H-%M"`
+		fname="${impl}_${jvm}_${time}.log"
+	fi
+	echo "$impl $jvm $args"
 	echo "$impl $jvm $args" > $fname
 	echo "make clean-java" >> $fname
 	make clean-java >> $fname 2>&1
@@ -36,5 +40,5 @@ while read impl jvm args; do
 	{ /usr/bin/time -v ${!jvm} -cp $BIN ${!impl} $args ; } >> $fname 2>&1
 done <$1
 
-echo "All work is done"
+echo -e "\nAll work is done\n"
 
