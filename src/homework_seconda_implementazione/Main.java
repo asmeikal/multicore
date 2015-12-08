84285b7df2d3d7ae2debfc30b4725b8f8d69ed6f
Michele Laurenti <asmeikal@me.com>
2015-12-08 18:44:29 +0100

    Clean whitespace

--- src/homework_seconda_implementazione/Main.java
+++ src/homework_seconda_implementazione/Main.java
@@ -8,26 +8,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
 
-        Path path = Paths.get(args[0]);
-        if (!Files.isRegularFile(path) || !path.toString().endsWith("txt")) {
-            System.out.println("Il file non è regolare oppure non è un file di testo");
-            return;
-        }
+		Path path = Paths.get(args[0]);
+		if (!Files.isRegularFile(path) || !path.toString().endsWith("txt")) {
+			System.out.println("Il file non è regolare oppure non è un file di testo");
+			return;
+		}
 
-        final String alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
-        int processors = Runtime.getRuntime().availableProcessors();
-        List<String> list = Files.readAllLines(path);
-        ArrayList<String> passwords = new ArrayList<String>(list);
+		final String alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+		int processors = Runtime.getRuntime().availableProcessors();
+		List<String> list = Files.readAllLines(path);
+		ArrayList<String> passwords = new ArrayList<String>(list);
 
-        Master master = new Master(passwords, alphabet, processors, 2);
-        Map<String,String> map = master.work();
+		Master master = new Master(passwords, alphabet, processors, 2);
+		Map<String, String> map = master.work();
 
-        for (String key : map.keySet()) {
-            System.out.println(key + " = MD5("+map.get(key)+")");
-        }
-    }
+		for (String key : map.keySet()) {
+			System.out.println(key + " = MD5(" + map.get(key) + ")");
+		}
+	}
 }
