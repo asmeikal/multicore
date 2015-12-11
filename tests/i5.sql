2d2c8b17f87975cc8bf82f20400dc34bacf30dbe
Michele Laurenti <asmeikal@me.com>
2015-12-11 22:57:06 +0100

    Add logs, sql digests, and graphics

---
+++ tests/i5.sql
@@ -0,0 +1,18 @@
+insert into test(piattaforma, implementazione, versione, nthread, tempo, nmaster, lpass, npass)
+values
+('i5', 1, 6, 1, '2835 seconds', 2, 6, 2),
+('i5', 1, 6, 2, '1779 seconds', 2, 6, 2),
+('i5', 1, 6, 3, '1413 seconds', 2, 6, 2),
+('i5', 1, 6, 4, '1296 seconds', 2, 6, 2),
+('i5', 1, 5, 1, '2695 seconds', 2, 6, 2),
+('i5', 1, 5, 2, '1494 seconds', 2, 6, 2),
+('i5', 1, 5, 3, '1565 seconds', 2, 6, 2),
+('i5', 1, 5, 4, '1579 seconds', 2, 6, 2),
+('i5', 2, 6, 1, '662 seconds', 2, 6, 2),
+('i5', 2, 6, 2, '377 seconds', 2, 6, 2),
+('i5', 2, 6, 3, '277 seconds', 2, 6, 2),
+('i5', 2, 6, 4, '231 seconds', 2, 6, 2),
+('i5', 2, 5, 1, '653 seconds', 2, 6, 2),
+('i5', 2, 5, 2, '362 seconds', 2, 6, 2),
+('i5', 2, 5, 3, '363 seconds', 2, 6, 2),
+('i5', 2, 5, 4, '364 seconds', 2, 6, 2);
